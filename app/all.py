7486d8189226b6e50601aa3fc2653ed9b79094f4
slickpockets <me@slickpockets.site>
2021-02-26 15:38:27 +0800

    celery task working sending statically sent phone number and values

--- app/all.py
+++ app/all.py
@@ -1,13 +1,21 @@
 from flask import current_app, Blueprint
 import os
-from .tasks import make_file
+from .tasks import make_file, requester
 
 bp = Blueprint("all", __name__)
 @bp.route("/")
 def index():
     return "hello"
+
 @bp.route("/<string:fname>/<string:content>")
 def makefile(fname, content):
     fpath = os.path.join(os.path.dirname(os.path.abspath(__file__)), fname)
     make_file.delay(fpath, content)
     return(f"find your file @ <code>{fpath}</code>")
+
+@bp.route("/test")
+def test():
+    message = "this is a test"
+    phonenumbers = "+61412145110"
+    requester.delay(message, phonenumbers)
+    return("done")
--- app/tasks.py
+++ app/tasks.py
@@ -1,5 +1,18 @@
 from app import celery
+import requests
+
 @celery.task()
 def make_file(fname, content):
     with open(fname, "w") as f:
         f.write(content)
+
+@celery.task()
+def requester(message, phonenumbers):
+    r = requests.post("http://10.0.0.8/sms/index.php")
+    data = {
+     "password" : "Prudence33",
+     "phonenumbers" : phonenumbers,
+     "message": message
+    }
+    print(phonenumbers, message)
+    r = requests.post("http://10.0.0.8/sms/index.php", data=data)
